b52583ac083ca61422350befbee35a8131c3ae04
zhugejunwei <zhugerock@gmail.com>
2016-01-30 19:10:08 -0500

    add shell sort with 2 loops

---
+++ Sort algorithms/Shell Sort.cpp
@@ -0,0 +1,46 @@
+//
+//  shell.cpp
+//
+//
+//  Created by 诸葛俊伟 on 1/30/16.
+//  Copyright © 2016 诸葛俊伟. All rights reserved.
+//
+
+#include <iostream>
+#include <vector>
+using namespace std;
+//typedef vector<int> number;
+
+void swap(int* x, int* y)
+{
+    int temp = *y;
+    *x = *y;
+    *y = temp;
+}
+
+void shell(int num[], int size)
+{
+    int gap, j;
+    for (gap = size/2; gap > 0; gap /= 2)
+        //for (i = gap; i < size; ++i)
+        for (j = 0; j <= size-gap+1 && num[j] > num [j+gap]; ++j)
+        {
+            swap(num[j], num[j+gap]);
+        }
+}
+
+int main()
+{
+    int num[] = {3,2,1,5,4,6,9,10};
+    int size = 8;
+    cout<<"original array:"<<endl;
+    for (int i = 0; i < size; ++i)
+        cout<<num[i]<<" ";
+    cout<<endl;
+    shell(num, size);
+    cout<<"shelled array:"<<endl;
+    for (int i = 0; i < size; ++i)
+        cout<<num[i]<<" ";
+    cout<<endl;
+    return 0;
+}
